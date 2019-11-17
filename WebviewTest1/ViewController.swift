5443673b71a92504cc0143e62017cc862c72fb90
J13281 <tuis.j13281@gmail.com>
2019-11-18 06:20:37 +0900

    Initial Commit

---
+++ WebviewTest1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebviewTest1
+//
+//  Created by 谷出広大 on 2019/11/18.
+//  Copyright © 2019 谷出広大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
